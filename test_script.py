0ddf82fd31e349b8d0cbd2ddf96a43eaad5b759c
mariaf494 <mariaf494@gmail.com>
2016-11-17 17:35:54 -0500

    Add files via upload

--- test_script.py
+++ test_script.py
@@ -2,13 +2,19 @@ import sys
 import pygame
 import pygame.camera
 import pygame.image
+import numpy as np
+import serial
+
 
 pygame.init()
 
 pygame.camera.init()
-cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+cam = pygame.camera.Camera(pygame.camera.list_cameras()[1])
 cam.start()
 img = cam.get_image()
-pygame.image.save(img,"tesfile.png")
+pygame.image.save(img,"yellow.png")
 pygame.camera.quit()
-
+imgdata=pygame.surfarray.array3d(img)
+arr=np.array(imgdata)
+arr=np.mean(np.mean(arr,axis=0),axis=0)
+print(arr)
